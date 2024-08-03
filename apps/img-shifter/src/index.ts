821d8b74454074a5feb2c1746c21720a5acf5b2b
Simon Diaz <simon.diaz.abad@gmail.com>
2024-08-03 15:07:05 -0400

    switched to yarn

--- apps/img-shifter/src/index.ts
+++ apps/img-shifter/src/index.ts
@@ -1,3 +1,8 @@
+if (true) {
+  console.log("Starting img-shifter microservice...");
+}
+console.log("Starting img-shifter microservice...");
+
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
@@ -9,4 +14,9 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("There was an uncaught error", err);
+  process.exit(1); //mandatory (as per the Node.js docs)
 });
