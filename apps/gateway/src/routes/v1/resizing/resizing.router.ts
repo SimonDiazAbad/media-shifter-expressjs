01c8d6a9a8fdc9f8ba7147a281d8d2fe1baf5950
Simon <simon.diaz.abad@gmail.com>
2024-08-24 19:28:33 -0400

    sent test msg

--- apps/gateway/src/routes/v1/resizing/resizing.router.ts
+++ apps/gateway/src/routes/v1/resizing/resizing.router.ts
@@ -1,10 +1,21 @@
 import express, { Express, Request, Response, Router } from "express";
+import amqp from "amqplib";
 
 const resizerRouter: Router = express.Router();
 
 // resizerRouter.get("/image-dimensions", imageDimensionsController);
-resizerRouter.get("/image-dimensions", (req: Request, res: Response) => {
-    res.send("image-dimensions");
+resizerRouter.get("/image-dimensions", async (req: Request, res: Response) => {
+  const connection = await amqp.connect("amqp://rabbitmq");
+  const channel = await connection.createChannel();
+  const queue = "TEST";
+
+  await channel.assertQueue(queue);
+  const message = "test msg";
+  channel.sendToQueue(queue, Buffer.from(message));
+
+  console.log(`sent to rabbitmq: ${message}`);
+
+  res.send("image-dimensions");
 });
 
 export default resizerRouter;
